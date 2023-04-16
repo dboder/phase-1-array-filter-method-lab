f4cdfbcc1c758ee9925aee6a2ff2bfa1edc854e9
Dorsey B <dorseybec@gmail.com>
2023-04-16 16:49:56 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+
+function softStringMatch(element, match ) {
+    return (element.toUpperCase() === match.toUpperCase());
+}
+function findMatching(arr,str) {
+    const newCollection = [];
+    for (const driver of arr) {
+        if (softStringMatch(driver,str)) {
+            newCollection.push(driver);
+        }
+    }
+    return newCollection;
+}
+
+function fuzzyMatch(arr,match) {
+    const newCollection = [];
+    for (const driver of arr) {
+        if (driver.slice(0,match.length) === match) {
+            newCollection.push(driver);
+        }
+    }
+    return newCollection;
+}
+
+function matchName(arr,match) {
+    const newCollection = [];
+    for (const driver of arr) {
+        if (driver.name.toUpperCase() === match.toUpperCase()) {
+            newCollection.push(driver);
+        }
+    }
+    return newCollection;
+}
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// console.log(findMatching(drivers,'bobby'));
